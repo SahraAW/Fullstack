a010327a312cec77b3a8e0f85ea1b4fd929ba5fc
Dr. Clocktor <jonas@sakejo.dk>
2024-11-13 10:59:36 +0100

    Tilføjet express, mysql2, cors samt lavet connection til databasen

--- Backend/Javascript/api/index.js
+++ Backend/Javascript/api/index.js
@@ -1,1 +1,15 @@
-console.log('Happy developing ✨')
+const express = require("express");
+const mysql = require("mysql2");
+const cors = require("cors");
+
+const app = express();
+const port = 3000;
+
+app.use(cors());
+
+const connection = db.createConnection({
+    host: process.env.DBHOST,
+    user: process.env.DBUSER,
+    password: process.env.DBPASSWORD,
+    database:"cafe_port"
+});
